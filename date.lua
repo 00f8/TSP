fde0e51cab1dde0b078819735f6c716753644863
00float8 <83893105+00f8@users.noreply.github.com>
2024-07-05 18:07:58 -0700

    Update date.lua

--- date.lua
+++ date.lua
@@ -1,6 +1,6 @@
-local targetDate = os.time({year=2023, month=7, day=22, hour=7, min=0, sec=0}) -- Change the date to when you want the countdown to end
+local targetDate = os.time({year=2024, month=7, day=22, hour=7, min=0, sec=0}) -- Change the date to when you want the countdown to end
 function updateCountdownDisplay()
- local Time = os.date("!*t", os.time() - 5* 60 * 60)  -- Change - 5 to whatever your timezone is (EG. New York = - 5)
+ local Time = os.date("!*t", os.time() - 7* 60 * 60)  
  local currentDate = os.time(Time)
  local secondsLeft = targetDate - currentDate
  local days = math.floor(secondsLeft / (24 * 60 * 60))
@@ -8,7 +8,7 @@ function updateCountdownDisplay()
  local minutes = math.floor((secondsLeft % (60 * 60)) / 60)
  local seconds = math.floor(secondsLeft % 60)
 
- game.Workspace.CountdownPart.SurfaceGui.CountdownText.Text = string.format("%dd %02dh %02dm %02ds", days, hours, minutes, seconds) -- Change CountdownPart to whatever your part is called which has the Countdown on it 
+ return string.format("%dd %02dh %02dm %02ds", days, hours, minutes, seconds) 
 end
 while true do
  updateCountdownDisplay()
