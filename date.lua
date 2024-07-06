9d8ccc9bfcb8e57839e148e19acf07e1c0622bd6
00float8 <83893105+00f8@users.noreply.github.com>
2024-07-05 18:18:45 -0700

    Update date.lua

--- date.lua
+++ date.lua
@@ -1,3 +1,4 @@
-local targetDate = os.time({year=2024, month=7, day=22, hour=7, min=0, sec=0})
+local Date = os.time({year=2024, month=7, day=22, hour=7, min=0, sec=0})
 
-print(targetDate)
+print(Date)
+return Date
