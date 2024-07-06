bd743f11ad0af8088b4b482414c425c93b742d9d
00float8 <83893105+00f8@users.noreply.github.com>
2024-07-05 19:03:56 -0700

    Update DropBoardAPI.lua

--- DropBoardAPI.lua
+++ DropBoardAPI.lua
@@ -1,4 +1,4 @@
-local targetDate = os.time({year=2024, month=7, day=6, hour=7, min=0, sec=0})
+local targetDate = os.time({year=2024, month=7, day=5, hour=22, min=0, sec=0})
 local Time = os.date("!*t", os.time() - 7 * 60 * 60)  -- Change - 5 to whatever your timezone is (EG. New York = - 5)
 local currentDate = os.time(Time)
 local secondsLeft = targetDate - currentDate
@@ -8,7 +8,7 @@ local minutes = math.floor((secondsLeft % (60 * 60)) / 60)
 local seconds = math.floor(secondsLeft % 60)
 	
 	
-workspace.DropBoard.Board.SurfaceGui.MessageLabel.Text = "Domino Crown"
-workspace.DropBoard.Board.SurfaceGui.ImageLabel.Image = "rbxthumb://type=Asset&id=1031429&w=420&h=420"
-workspace.DropBoard.Board.SurfaceGui.InfoLabel.Text = "Copies: 25, Price: 100,000 "
+workspace.DropBoard.Board.SurfaceGui.MessageLabel.Text = "Eccentric Shop Teacher"
+workspace.DropBoard.Board.SurfaceGui.ImageLabel.Image = "rbxthumb://type=Asset&id=26943368&w=420&h=420"
+workspace.DropBoard.Board.SurfaceGui.InfoLabel.Text = "Copies: 10, Price: 100,000"
 workspace.DropBoard.Board.SurfaceGui.DateLabel.Text = string.format("%dd %02dh %02dm %02ds", days, hours, minutes, seconds)
