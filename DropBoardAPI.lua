adaed52d1b5e875ab60835bce6e40d046e18df11
00float8 <83893105+00f8@users.noreply.github.com>
2024-07-06 03:04:55 -0700

    Update DropBoardAPI.lua

--- DropBoardAPI.lua
+++ DropBoardAPI.lua
@@ -1,4 +1,4 @@
-local targetDate = os.time({year=2024, month=7, day=5, hour=22, min=0, sec=0})
+local targetDate = os.time({year=2024, month=7, day=6, hour=16, min=0, sec=0})
 local Time = os.date("!*t", os.time() - 7 * 60 * 60)  -- Change - 5 to whatever your timezone is (EG. New York = - 5)
 local currentDate = os.time(Time)
 local secondsLeft = targetDate - currentDate
@@ -8,9 +8,9 @@ local minutes = math.floor((secondsLeft % (60 * 60)) / 60)
 local seconds = math.floor(secondsLeft % 60)
 	
 	
-workspace.DropBoard.Board.SurfaceGui.MessageLabel.Text = "Eccentric Shop Teacher"
-workspace.DropBoard.Board.SurfaceGui.ImageLabel.Image = "rbxthumb://type=Asset&id=26943368&w=420&h=420"
-workspace.DropBoard.Board.SurfaceGui.InfoLabel.Text = "Copies: 10, Price: 100,000"
+workspace.DropBoard.Board.SurfaceGui.MessageLabel.Text = "Bluesteel Domino Crown"
+workspace.DropBoard.Board.SurfaceGui.ImageLabel.Image = "rbxthumb://type=Asset&id=68258723&w=420&h=420"
+workspace.DropBoard.Board.SurfaceGui.InfoLabel.Text = "Copies: 150, Price: 150"
 workspace.DropBoard.Board.SurfaceGui.DateLabel.Text = string.format("%dd %02dh %02dm %02ds", days, hours, minutes, seconds)
 
 if secondsLeft <= 0 then
