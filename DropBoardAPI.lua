fb368249641ad3f98fc2ec5456f10b1b2ad9e5fb
00float8 <83893105+00f8@users.noreply.github.com>
2024-07-05 18:29:18 -0700

    Update DropBoardAPI.lua

--- DropBoardAPI.lua
+++ DropBoardAPI.lua
@@ -1,4 +1,4 @@
-local targetDate = os.time({year=2024, month=7, day=22, hour=7, min=0, sec=0})
+local targetDate = os.time({year=2024, month=7, day=6, hour=7, min=0, sec=0})
 local Time = os.date("!*t", os.time() - 7 * 60 * 60)  -- Change - 5 to whatever your timezone is (EG. New York = - 5)
 local currentDate = os.time(Time)
 local secondsLeft = targetDate - currentDate
